e8828f323bb3a8bfda477924a63eb806797ff6f4
Mike Hickman <pdxmike@users.noreply.github.com>
2017-01-17 20:13:52 -0800

    add ability to create Posts with form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,33 @@
 class PostsController < ApplicationController
-  def index
+  before_action :post_find, only: [:show, :update, :edit, :destroy]
 
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post), notice: 'Your post was created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def post_find
+    @post = Post.find(params[:id])
   end
 
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,14 +2,35 @@ require 'rails_helper'
 
 describe 'Navigate' do
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  describe 'Creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'has a form that can be reached at posts/new' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some Rationale'
+
+      click_on 'Save'
+
+      expect(page).to have_content('Some Rationale')
     end
   end
 end
