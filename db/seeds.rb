b694d558f5616e952e2a94f659cc53eb219154be
Mike Hickman <pdxmike@users.noreply.github.com>
2017-01-26 11:50:41 -0800

    add ability to edit post status from admin dashboard form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 @user = User.create(email: 'test@test.com', password: 'testing', password_confirmation: 'testing', first_name: 'Jon', last_name: 'Snow')
-
 puts '1 user created'
+
+AdminUser.create(email: 'admin@test.com', password: 'testing', password_confirmation: 'testing', first_name: 'Admin', last_name: 'Ruler')
+puts '1 admin user created'
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
