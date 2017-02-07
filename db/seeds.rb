1e7b399e441d6bcb8109319bfb66204b04e94408
Mike Hickman <pdxmike@users.noreply.github.com>
2017-02-07 11:24:25 -0800

    update db seed to include overtime hours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ AdminUser.create(email: 'admin@test.com', password: 'testing', password_confirma
 puts '1 admin user created'
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", overtime_request: 2.5, user_id: @user.id)
 end
 
 puts "100 Posts have been created!"
