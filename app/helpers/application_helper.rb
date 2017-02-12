137b87b007775a3afecafc4fee4b78c9adb01eac
Mike Hickman <pdxmike@users.noreply.github.com>
2017-02-12 08:35:53 -0800

    add enums for audit log status and updated labels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,24 @@ module ApplicationHelper
     'active' if current_page?(path)
   end
 
+  def label_status status
+    status_span_generator status
+  end
+
+  private
+    def status_span_generator status
+      case status
+      when 'submitted'
+        content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'approved'
+        content_tag(:span, status.titleize, class: 'label label-success')
+      when 'rejected'
+        content_tag(:span, status.titleize, class: 'label label-danger')
+      when 'pending'
+          content_tag(:span, status.titleize, class: 'label label-primary')
+      when 'confirmed'
+          content_tag(:span, status.titleize, class: 'label label-success')
+      end
+    end
+
 end
