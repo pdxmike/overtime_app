1fb3300b88a81bf820bfa6728cdcec5879e419f4
Mike Hickman <pdxmike@users.noreply.github.com>
2017-02-19 19:16:00 -0800

    add callback to update audit log when OT requested

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,19 @@ class Post < ActiveRecord::Base
   validates :overtime_request, numericality: { greater_than: 0.0 }
 
   scope :posts_by, ->(user) { where(user_id: user.id) }
+
+  after_save :confirm_audit_log, if: :submitted?
+	after_save :un_confirm_audit_log, if: :rejected?
+
+	private
+
+		def confirm_audit_log
+			audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			audit_log.confirmed! if audit_log
+		end
+
+		def un_confirm_audit_log
+			audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+			audit_log.pending! if audit_log
+		end
 end
