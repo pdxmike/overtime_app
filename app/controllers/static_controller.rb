6c86c1b23929c3dd7104cf1581b4d2d938a38960
Mike Hickman <pdxmike@users.noreply.github.com>
2017-02-19 17:29:06 -0800

    style employee pending confirmations on home page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.where(status: 'submitted')
-    @recent_audit_items = AuditLog.last(12)
+  	if admin_types.include?(current_user.type)
+    	@pending_approvals = Post.submitted
+    	@recent_audit_items = AuditLog.last(12)
+    else
+    	@employee_pending_confirmations = current_user.audit_logs
+    end
   end
 end
