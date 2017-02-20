c3737645bc75004e32ca3864c29763113ffa6a77
Mike Hickman <pdxmike@users.noreply.github.com>
2017-02-20 13:40:59 -0800

    fix heroku server not finding prod secret key base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Fix heroku error of not finding production secret key base
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
