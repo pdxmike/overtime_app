d75b617f289fcb2a701a3cd54442b0351090da93
Mike Hickman <pdxmike@users.noreply.github.com>
2017-03-01 09:53:05 -0800

    add self referential table - employees & managers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212140811) do
+ActiveRecord::Schema.define(version: 20170301175135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170212140811) do
   end
 
   add_index "audit_logs", ["user_id"], name: "index_audit_logs_on_user_id", using: :btree
+
+  create_table "hands", force: :cascade do |t|
+    t.integer  "hand_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
